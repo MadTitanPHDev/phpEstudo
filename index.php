b6b1b38ef46777fddf59e8692b2aa941d5234188
PAULO HENRIQUE MARGUTTI JUNIOR <paulo.hmjunior2@senacsp.edu.br>
2024-05-03 21:37:23 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post_PHP</title>
+    <link rel="stylesheet" href="./assets/css/post_php.css">
+    <link rel="icon" type="image/png" href="./assets/icones/PHP_Logo.png">
+</head>
+<body>
+    <head></head>
+
+    <main></main>
+
+    <footer></footer>
+    
+</body>
+</html>
